87333b2974de6a75bfe5ae7abc8bb3420d8c0fe8
Yan <fzerix@gmail.com>
2024-03-14 09:36:23 +0300

    add adaptive (trying)

--- src/components/field/field.tsx
+++ src/components/field/field.tsx
@@ -3,6 +3,8 @@ import Cell from "../cell/cell";
 import { Link } from "react-router-dom";
 import isMovePossible from "./isMovePossible";
 
+import { useSwipeable } from 'react-swipeable';
+
 export default function Field({ size }: { size: number }) {
 
   const defaultArray = Array.from({ length: size }, () =>
@@ -41,6 +43,21 @@ export default function Field({ size }: { size: number }) {
       addRandomTile(newGrid);
     }
   };
+
+  const handleSwipe = (direction: string) => {
+    const newGrid = moveTiles(direction, [...grid]);
+    if (isGridChanged(grid, newGrid)) {
+      setGrid(newGrid);
+      addRandomTile(newGrid);
+    }
+  };
+
+  const swipeHandlers = useSwipeable({
+    onSwipedLeft: () => handleSwipe('ArrowLeft'),
+    onSwipedRight: () => handleSwipe('ArrowRight'),
+    onSwipedUp: () => handleSwipe('ArrowUp'),
+    onSwipedDown: () => handleSwipe('ArrowDown'),
+  });
 
   const moveTiles = (direction: string, currentGrid: number[][]) => {
     const newGrid = JSON.parse(JSON.stringify(currentGrid)); // Создаем копию сетки
@@ -164,91 +181,7 @@ export default function Field({ size }: { size: number }) {
   useEffect(() => {
     window.addEventListener("keydown", handleKeyPress);
 
-    const moveTiles = (direction: string, currentGrid: number[][]) => {
-      const newGrid = JSON.parse(JSON.stringify(currentGrid)); // Создаем копию сетки
-
-      const move = (
-        row: number,
-        col: number,
-        rowModifier: number,
-        colModifier: number
-      ) => {
-        const currentValue = newGrid[row][col];
-
-        if (currentValue === 0) {
-          return; // Ничего не делаем, если клетка пуста
-        }
-
-        let newRow = row;
-        let newCol = col;
-
-        while (
-          newRow + rowModifier >= 0 &&
-          newRow + rowModifier < size &&
-          newCol + colModifier >= 0 &&
-          newCol + colModifier < size &&
-          newGrid[newRow + rowModifier][newCol + colModifier] === 0
-        ) {
-          // Перемещаем клетку в направлении, пока не достигнем границы или другой клетки
-          newGrid[newRow + rowModifier][newCol + colModifier] =
-            newGrid[newRow][newCol];
-          newGrid[newRow][newCol] = 0;
-          newRow += rowModifier;
-          newCol += colModifier;
-        }
-
-        if (
-          newRow + rowModifier >= 0 &&
-          newRow + rowModifier < size &&
-          newCol + colModifier >= 0 &&
-          newCol + colModifier < size &&
-          newGrid[newRow + rowModifier][newCol + colModifier] === currentValue
-        ) {
-          // Слияние, если достигнута клетка с тем же значением
-          newGrid[newRow + rowModifier][newCol + colModifier] += currentValue;
-          newGrid[newRow][newCol] = 0;
-        }
-      };
-
-      switch (direction) {
-        case "ArrowUp":
-        case "w":
-          for (let col = 0; col < size; col++) {
-            for (let row = 1; row < size; row++) {
-              move(row, col, -1, 0);
-            }
-          }
-          break;
-        case "ArrowDown":
-        case "s":
-          for (let col = 0; col < size; col++) {
-            for (let row = size - 2; row >= 0; row--) {
-              move(row, col, 1, 0);
-            }
-          }
-          break;
-        case "ArrowLeft":
-        case "a":
-          for (let row = 0; row < size; row++) {
-            for (let col = 1; col < size; col++) {
-              move(row, col, 0, -1);
-            }
-          }
-          break;
-        case "ArrowRight":
-        case "d":
-          for (let row = 0; row < size; row++) {
-            for (let col = size - 2; col >= 0; col--) {
-              move(row, col, 0, 1);
-            }
-          }
-          break;
-        default:
-          break;
-      }
-
-      return newGrid;
-    };
+    window.addEventListener("")
 
     const checkMovesAfterRender = () => {
       if (!isMovePossible(size, grid)) {
@@ -265,23 +198,23 @@ export default function Field({ size }: { size: number }) {
   }, [grid]);
 
   return (
-    <div className="relative content">
+    <div className="relative content" {...swipeHandlers}>
       <div className="flex items-center justify-center w-screen h-screen">
         <div>
           <h1
-            className="absolute transform -translate-x-1/2 left-1/2"
+            className="absolute text-2xl transform -translate-x-1/2 left-1/2 sm:text-3xl md:text-4xl lg:text-5xl"
             style={{ top: `${titleTop}px` }}
           >
             {titleText}
           </h1>
 
           <div
-            className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#030914] -z-10 rounded-lg shadow-[0_0_35px_3px_#483179]`}
+            className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#030914] -z-10 rounded-lg shadow-[0_0_35px_3px_#483179] sm:w-64 sm:h-64 md:w-72 md:h-72 lg:w-80 lg:h-80`}
             style={{ width: `${squareSize}px`, height: `${squareSize}px` }}
           ></div>
 
           <div
-            className="grid"
+            className="grid sm:gap-2 md:gap-3 lg:gap-4"
             style={{
               gridTemplateColumns: `repeat(${size}, minmax(0, 1fr))`,
               gap: `${gap}px`,
@@ -299,23 +232,25 @@ export default function Field({ size }: { size: number }) {
             )}
           </div>
 
-          
-
           {titleText == 'Game over' ? (
-            <button onClick={() => 
-              {setGrid(defaultArray); 
-               setTitleText('2048');
-               startTiles(defaultArray);
+            <button
+              onClick={() => {
+                setGrid(defaultArray);
+                setTitleText('2048');
+                startTiles(defaultArray);
               }}
-            className='absolute flex items-center justify-center -translate-x-1/2 bottom-10 left-1/2 text-center bg-[#1e1432] w-96 h-20 rounded-xl select-none hover:bg-[#19112b] text-[48px]'>
-            Restart
-           </button>
+              className="absolute flex items-center justify-center -translate-x-1/2 bottom-10 left-1/2 text-center bg-[#1e1432] w-72 sm:w-80 md:w-96 h-16 sm:h-20 rounded-xl select-none hover:bg-[#19112b] text-2xl sm:text-3xl md:text-4xl lg:text-5xl"
+            >
+              Restart
+            </button>
           ) : (
-            <Link to='/' className='absolute flex items-center justify-center -translate-x-1/2 bottom-8 left-1/2 text-center bg-[#1e1432] w-96 h-20 rounded-xl select-none hover:bg-[#19112b] text-[48px]'>
-             Back
-           </Link>
+            <Link
+              to="/"
+              className="absolute flex items-center justify-center -translate-x-1/2 bottom-8 left-1/2 text-center bg-[#1e1432] w-72 sm:w-80 md:w-96 h-16 sm:h-20 rounded-xl select-none hover:bg-[#19112b] text-2xl sm:text-3xl md:text-4xl lg:text-5xl"
+            >
+              Back
+            </Link>
           )}
-
         </div>
       </div>
     </div>
--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -4,10 +4,10 @@ const Menu = () => {
   return (
     <div className="relative content">
       <div className="flex items-center justify-center w-screen h-screen">
-        <h1 className="absolute -translate-x-1/2 top-[135px] left-1/2">
+        <h1 className="absolute sm:text-[12px] md:text-[16px] -translate-x-1/2 sm:top-[24px] md:top-[36px] lg:top-[135px] left-1/2">
           Выберите размер поля
         </h1>
-        <div className="grid grid-cols-3 gap-[64px]">
+        <div className="grid sm:grid-cols-1 lg:grid-cols-3 sm:gap-[12px] lg:gap-[64px]">
           <Button size={3} />
           <Button size={4} />
           <Button size={5} />
